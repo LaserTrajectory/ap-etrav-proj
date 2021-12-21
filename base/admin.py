7df8610bd236782ef5397e8f73f87efd96e11a1a
Aniruddh Bhaskaran <aniruddh.v.bhaskaran@gmail.com>
2021-12-21 21:13:45 +0530

    hotel page ui update

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Location, Hotel, HotelRoom, RoomBooking
+from .models import Location, Hotel, HotelRoom, RoomBooking, HotelReview
 # Register your models here.
 
 admin.site.register(Location)
 admin.site.register(Hotel)
 admin.site.register(HotelRoom)
 admin.site.register(RoomBooking)
+admin.site.register(HotelReview)
